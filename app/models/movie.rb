80ebb0e8108657998bf6d9e09bd8c082a0536479
joe mcilroy <joeamcilroy@gmail.com>
2022-02-17 12:42:54 +0000

    validates models

---
+++ app/models/movie.rb
@@ -0,0 +1,7 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+ # A movie must have a unique title and an overview.
+
+end
