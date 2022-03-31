51fd318ce5cf2522cbece49ef678f0531a911d73
joe mcilroy <joeamcilroy@gmail.com>
2022-03-31 11:57:08 +0100

    show card rating & layout

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,6 @@
 class Bookmark < ApplicationRecord
   has_one_attached :photo
+  validates :photo, presence: true
   belongs_to :movie
   belongs_to :list
   validates :comment, presence: true, length: { minimum: 6 }
